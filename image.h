2e60d9f001d08b4a745625f3aebe522376bff74e
Matthew McPherrin <matthew@mcpherrin.ca>
2011-12-05 11:37:45 -0800

    image

---
+++ image.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <unistd.h>
+
+struct colour {
+  char red;
+  char green;
+  char blue;
+} __attribute__((packed));
+
+/* Represents an uncompressed image */
+struct image {
+  /* How many colours are in the image*/
+  int colours;
+  /* What the colours are */
+  struct colour *colourtable;
+  int width;
+  int height;
+  char *data;
+};
+
+size_t encodeGIF(struct image* image, unsigned char *output);
+void render(char *line, int colour, int top, int left, struct image *image);
